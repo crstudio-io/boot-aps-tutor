fff6219aa7849970d89baa6af5b59b6c247f26e2
aquashdw <aquashdw@gmail.com>
2024-06-12 21:21:34 +0900

    do not serialize PageImpl directly

---
+++ src/main/kotlin/io/crstudio/tutor/config/SpringDataConfig.kt
@@ -0,0 +1,9 @@
+package io.crstudio.tutor.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.web.config.EnableSpringDataWebSupport
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO
+
+@Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
+class SpringDataConfig
