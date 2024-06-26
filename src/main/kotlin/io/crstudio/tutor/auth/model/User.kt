3c5c8d3a2ef31b5b50ce916a5b4a723efdcc4e07
aquashdw <aquashdw@gmail.com>
2024-06-26 12:23:35 +0900

    add role

--- src/main/kotlin/io/crstudio/tutor/auth/model/User.kt
+++ src/main/kotlin/io/crstudio/tutor/auth/model/User.kt
@@ -8,5 +8,25 @@ class User(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long?,
-    val email: String?
+    val email: String?,
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+        name = "USER_ROLES",
+        joinColumns = [JoinColumn(name = "USER_ID", referencedColumnName = "id")],
+        inverseJoinColumns = [JoinColumn(name = "ROLE_ID", referencedColumnName = "id")]
+    )
+    val roles: MutableSet<Role> = mutableSetOf(),
 )
+
+@Entity
+@Table(name = "ROLE_TABLE")
+class Role(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long?,
+    val name: String,
+) {
+    override fun toString(): String {
+        return "ROLE_$name"
+    }
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,6 @@
 insert into USER_TABLE(email) values ('aquashdw@gmail.com');
+insert into ROLE_TABLE(name) values ('ADMIN'), ('MAINTAINER'), ('USER');
+insert into USER_ROLES(user_id, role_id) values (1, 1);
 
 insert into PROBLEM(user_id, prob_desc, input_desc, output_desc, timeout, memory)
 values (1, '간단한 입출력을 연습하자.', '한줄의 데이터가 입력으로 주어진다.', '해당 데이터를 아무 변경 없이 출력한다.', 10, 512);
