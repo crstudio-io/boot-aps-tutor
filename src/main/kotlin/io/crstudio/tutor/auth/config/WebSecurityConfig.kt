8e9981d8a80d6a6bcfde1adf78358696d8306d35
aquashdw <aquashdw@gmail.com>
2024-06-27 22:26:17 +0900

    add front host for cors

--- src/main/kotlin/io/crstudio/tutor/auth/config/WebSecurityConfig.kt
+++ src/main/kotlin/io/crstudio/tutor/auth/config/WebSecurityConfig.kt
@@ -3,6 +3,7 @@ package io.crstudio.tutor.auth.config
 import io.crstudio.tutor.auth.jwt.JwtFilter
 import io.crstudio.tutor.auth.jwt.JwtUtils
 import io.crstudio.tutor.auth.repo.UserRepo
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
@@ -14,17 +15,25 @@ import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.access.intercept.AuthorizationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 class WebSecurityConfig(
     val jwtUtils: JwtUtils,
     val userRepo: UserRepo,
+    @Value("\${service.front-host}")
+    val frontOrigin: String,
 ) {
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         http {
             csrf {
                 disable()
+            }
+            cors {
+                configurationSource = corsConfigurationSource()
             }
             authorizeHttpRequests {
                 authorize("/auth/signin", permitAll)
@@ -55,5 +64,16 @@ class WebSecurityConfig(
         template.valueSerializer = RedisSerializer.json()
         template.setEnableTransactionSupport(true)
         return template
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf(frontOrigin)
+        configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE")
+        configuration.allowedHeaders = listOf("*")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
     }
 }
